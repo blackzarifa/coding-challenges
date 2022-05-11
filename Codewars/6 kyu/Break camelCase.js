07fb7f9efd56fe9393959d73f9fd164edcefdeeb
Rafael Irades <rafaelirades@gmail.com>
2022-05-11 17:43:14 -0300

    Added more solutions and changed file names

---
+++ Codewars/6 kyu/Break camelCase.js
@@ -0,0 +1,15 @@
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+    return string
+      .split('')
+      .map(letter => /[A-Z]/.test(letter) ? (' ' + letter) : letter)
+      .join('');
+}
